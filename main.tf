105b2e71ead346daa905dbecdf2deebebf03dd2a
Kelvin Glaß <7346610+m273d15@users.noreply.github.com>
2022-07-12 15:50:01 +0200

    crossplane minio example

--- main.tf
+++ main.tf
@@ -1,13 +1,36 @@
-variable "content" {
+terraform {
+  required_providers {
+    minio = {
+      version = "0.1.0"
+      source  = "refaktory/minio"
+    }
+  }
+}
+
+variable "endpoint" {
   type = string
 }
 
-variable "path" {
+variable "bucket_name" {
   type = string
-  default = "/tmp/test.txt"
 }
 
-resource "local_file" "test" {
-  content = var.content
-  filename = var.path
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
+provider "minio" {
+  endpoint = var.endpoint
+  access_key = var.access_key
+  secret_key = var.secret_key
+  ssl = false
+}
+
+
+resource "minio_bucket" "bucket" {
+  name = var.bucket_name
 }
